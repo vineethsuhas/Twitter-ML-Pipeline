300d506149bf69eea660364d244d41e6654f3a37
vineethsuhas <vineethsuhas@gmail.com>
2020-06-28 16:52:29 -0400

    Add STATIC_ROOT in settings

--- emotiClass/emotiClass/settings.py
+++ emotiClass/emotiClass/settings.py
@@ -150,6 +150,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 TWITTER_KAFKA_TOPIC_PREFIX = "tweets_"
 
